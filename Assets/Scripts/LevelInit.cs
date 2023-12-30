d2b7060dc0d0bcf2e70e01a64cf2b15c546be8b9
Tomas Bukovy <t_bukovy@utb.cz>
2023-12-30 20:33:35 +0100

    Pridani generovani mapy ... castecne

--- Assets/Scripts/LevelInit.cs
+++ Assets/Scripts/LevelInit.cs
@@ -1,22 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class LevelInit : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] private GameObject emptyPrefab;
+    [SerializeField] private GameObject wallPrefab;
     void Start()
     {
-        int[,] randMap = {
+        int[,] randMap1 = {
             {0, 0, 0, 0, 0},
             {0, 1, 1, 1, 0},
             {0, 1, 0, 1, 0},
             {0, 1, 1, 1, 0},
             {0, 0, 0, 0, 0}
         };
+        int[,] randMap2 = {
+            {0, 1, 1, 1, 0},
+            {0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0},
+            {0, 1, 1, 1, 0}
+        };
+        int[,] randMap3 = {
+            {1, 1, 1, 1, 0},
+            {1, 0, 0, 1, 0},
+            {1, 0, 0, 1, 0},
+            {1, 0, 0, 1, 0},
+            {1, 1, 1, 1, 0}
+        };
+
+        int[,] randMap4 = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+
+        System.Random rnd = new System.Random();
+        int mapa = rnd.Next(1, 4);
+        Debug.Log(mapa);
 
         MapGenerator mp = new MapGenerator(); //zavolám instanci tøídy MapGenerator a pøedám random matici pro mapu
-        mp.GenerateMap(randMap);
+        mp.GenerateMap(randMap1, emptyPrefab, wallPrefab);
+        switch (mapa)
+        {
+            case 1:
+                mp.GenerateMap(randMap1, emptyPrefab, wallPrefab);
+                break;
+            case 2:
+                mp.GenerateMap(randMap2, emptyPrefab, wallPrefab);
+                break;
+            case 3:
+                mp.GenerateMap(randMap3, emptyPrefab, wallPrefab);
+                break;
+            case 4:
+                mp.GenerateMap(randMap4, emptyPrefab, wallPrefab);
+                break;
+
+        }
         Debug.Log("Volám generátor...");
     }
 
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,34 +1,27 @@
 using UnityEngine;
 
-
-[RequireComponent(typeof(GameObject))]
-[RequireComponent(typeof(GameObject))]
-
 public class MapGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public GameObject emptySpacePrefab;
-    public GameObject wallPrefab;
-
-    public void GenerateMap(int[,] map)
+    float spacing = 1f;
+    public void GenerateMap(int[,] map,GameObject emptySpacePrefab, GameObject wallPrefab)
     {
-        //for (int i = 0; i < map.GetLength(0); i++)
-        //{
-        //    for (int j = 0; j < map.GetLength(1); j++)
-        //    {
-        //        Vector3 position = new Vector3(i, 0, j);
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Vector3 position = new Vector3(i * spacing, j * spacing,0);
 
-        //        if (map[i, j] == 0)
-        //        {
-        //            Instantiate(emptySpacePrefab, position, Quaternion.identity);
-        //        }
-        //        else if (map[i, j] == 1)
-        //        {
-        //            Instantiate(wallPrefab, position, Quaternion.identity);
-        //        }
-        //    }
-        //}
+                if (map[i, j] == 0)
+                {
+                    Instantiate(emptySpacePrefab, position, Quaternion.identity);
+                }
+                else if (map[i, j] == 1)
+                {
+                    Instantiate(wallPrefab, position, Quaternion.identity);
+                }
+            }
+        }
         Debug.Log("Mapa done...");
     }
 
