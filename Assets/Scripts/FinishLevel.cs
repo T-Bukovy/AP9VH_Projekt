92659db38a1e81782deffdfce5681d424ade9fe3
Tomas Bukovy <t_bukovy@utb.cz>
2023-12-30 16:48:54 +0100

    Přdání načítaní nového levelu

---
+++ Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            CompleteLevel();
+        }
+    }
+
+    private void CompleteLevel()
+    {
+        Debug.Log("Loading next level...");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
